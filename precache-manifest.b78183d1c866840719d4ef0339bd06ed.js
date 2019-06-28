a4606292ea3753db96c8224e7623b19f634e6a13
unknown <lastsaintcrucified@gmail.com>
2019-06-28 08:21:30 +0600

    Updates

--- precache-manifest.b78183d1c866840719d4ef0339bd06ed.js
+++ precache-manifest.b78183d1c866840719d4ef0339bd06ed.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0404c9f5833de514b1bea9f9c21db038",
+    "revision": "f1f7f2f6679719861e8d9d1fbda2bfd5",
     "url": "/react-contact-manager/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-contact-manager/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "5976968b0c0ce40f2b0c",
+    "revision": "cfa28f8d5257156ae997",
     "url": "/react-contact-manager/static/css/main.1fd9cdef.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react-contact-manager/static/js/2.f6885ec4.chunk.js"
   },
   {
-    "revision": "5976968b0c0ce40f2b0c",
-    "url": "/react-contact-manager/static/js/main.49051ceb.chunk.js"
+    "revision": "cfa28f8d5257156ae997",
+    "url": "/react-contact-manager/static/js/main.38bfbbdd.chunk.js"
   },
   {
     "revision": "817909a50868ee2241bd",
