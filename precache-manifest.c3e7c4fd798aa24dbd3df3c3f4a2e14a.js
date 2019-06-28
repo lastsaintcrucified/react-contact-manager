6fe3264111095dcd8bfbf10ca526d6886f82a72e
unknown <lastsaintcrucified@gmail.com>
2019-06-28 08:10:56 +0600

    Updates

---
+++ precache-manifest.c3e7c4fd798aa24dbd3df3c3f4a2e14a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0404c9f5833de514b1bea9f9c21db038",
+    "url": "/react-contact-manager/index.html"
+  },
+  {
+    "revision": "a1b33cb1914cc3a1c179",
+    "url": "/react-contact-manager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "5976968b0c0ce40f2b0c",
+    "url": "/react-contact-manager/static/css/main.1fd9cdef.chunk.css"
+  },
+  {
+    "revision": "a1b33cb1914cc3a1c179",
+    "url": "/react-contact-manager/static/js/2.f6885ec4.chunk.js"
+  },
+  {
+    "revision": "5976968b0c0ce40f2b0c",
+    "url": "/react-contact-manager/static/js/main.49051ceb.chunk.js"
+  },
+  {
+    "revision": "817909a50868ee2241bd",
+    "url": "/react-contact-manager/static/js/runtime~main.f3ab48b1.js"
+  },
+  {
+    "revision": "8262057119c963741b868e0d8dd68dd3",
+    "url": "/react-contact-manager/static/media/economica-bold.82620571.ttf"
+  },
+  {
+    "revision": "cf4449c1884598b292630aa9f4007c19",
+    "url": "/react-contact-manager/static/media/roboto-condensed.light.cf4449c1.ttf"
+  }
+]);
